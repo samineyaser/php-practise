0a34c01423386c170dab0b778366bffd45c735bc
samineyaser <samineyasers@gmail.com>
2023-02-28 02:21:25 +0600

    OOP Structure

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-// In this File we will learn more about string
-// Single quoted
-$name = "Hello samin this is me, My name is PHP";
-// $arraytostr = implode(" ", '$name');
-$newName = strpos($name, 'is');
-echo $newName;
-
-// echo substr($name, 24, -5);
+echo "Hello, World!";
---
+++ templates/oop.php
@@ -0,0 +1,41 @@
+<?php
+// Basic class and object
+// class Human //class body below
+// {
+//  public $name;//class property
+//  public function sayHello()//class method
+//  {
+//   echo "Hello, {$this->name}\n";//accessing the property using this keyword
+//  }
+// }
+// $h1       = new Human();// Creating first object using class
+// $h2       = new Human();// Creating Second object using class
+// $h1->name = "samin"; // set the property value
+// $h2->name = "eyaser"; // set the property value
+// $h1->sayHello();//excuting the object
+// $h2->sayHello();
+// echo $h1->name; //geting the value from class
+
+// Construct function
+// class Human
+// {
+//  public $name;
+//  public $age;
+//  public function __construct($personname, $personAge)
+//  {
+//   $this->name = $personname;
+//   $this->age  = $personAge;
+//  }
+//  public function sayHello()
+//  {
+//   $this->sayname();
+//  }
+//  public function sayname()
+//  {
+//   echo "Hello my name is {$this->name}, and my age is {$this->age}\n";
+//  }
+// }
+// $h1 = new Human("samin", 24); //set
+// $h2 = new Human("Insana", 21); //set
+// $h1->sayHello();
+// $h2->sayHello();
